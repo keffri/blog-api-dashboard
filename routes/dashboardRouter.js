fa1b7ef9fce70069d48938e7828d4523665a017f
keffri <keffri.w.neal@gmail.com>
2022-10-21 10:59:16 -0700

    comment delete

--- routes/dashboardRouter.js
+++ routes/dashboardRouter.js
@@ -6,4 +6,14 @@ const dashboard_controller = require('../controllers/dashboardController');
 // DASHBOARD
 router.get('/', dashboard_controller.getDashboard);
 
+// POSTS
+router.get('/posts', dashboard_controller.getPosts);
+router.get('/posts/:post_id', dashboard_controller.getPost);
+
+// COMMENTS
+router.delete(
+  '/posts/:post_id/comments/:comment_id/',
+  dashboard_controller.deleteComment
+);
+
 module.exports = router;
